1786cf1ad92a596a440c1deb239deaaefe5bc1a4
Lee <windstar1478@users.noreply.github.com>
2026-03-30 21:19:00 +0900

    feat(day 5): implement create and read-all logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 //main.cpp
-
 #include <iostream>
+#include <vector>
+#include <string>
 #include "httplib.h"
 using namespace std;
 
-void build_response(httplib::Response& res, int status_code) { //상태 코드에 따라 HTTP 응답 내용을 설정하는 함수
-	if (status_code == 200) {
-		res.set_content("Server On", "text/plain"); //응답: "Server On" (뒤의 "text/plain": 일반 텍스트)
-		res.status = 200; //요청이 정상적으로 인증 조건 통과 
-	}
-	else if (status_code == 401) {
-		res.set_content("Unauthorized", "text/plain");
-		res.status = 401; //인증 실패 (Unauthorized)
-	}
+
+struct AlbumRecord
+{
+	int id;
+	string date;
+	string album;
+	string artist;
+
+	AlbumRecord(int id, const string& date, const string& album, const string &artist) 
+		: id(id), date(date), album(album), artist(artist) {}
+};
+vector<AlbumRecord> AlbumRecords;	
+int next_id = 1; //다음으로 저장될 id 번호
+
+void build_response(httplib::Response& res, int status_code, 
+	const string& log_line, const string& body) { //상태 코드에 따라 HTTP 응답 내용을 설정하는 함수
+	cout << log_line << endl;
+
+	res.status = status_code;
+	res.set_content(body, "text/plain");
 }
 
 int main() {
@@ -21,27 +33,83 @@ int main() {
 	httplib::Server server; //서버 객체 생성
 
 	server.Get("/health", [](const httplib::Request& req, httplib::Response& res) { //'/health'로 GET 요청이 오면 이 코드 실행
-		cout << "[REQ] GET /health ";//GET 요청 로그 출력
+		cout << "[REQ] GET /health" << endl;//GET 요청 로그 출력
 
 		if (req.has_param("token")) { //token이 존재하는지 확인
 			string token = req.get_param_value("token"); //사용자가 제공한 token값 조회 후 (sting) token에 저장
-			cout << "token = " << token << endl;
+			cout << "token: " << token << endl;
 
 			if (token == "abc") { //만약 제공된 token 값이 "abc"와 일치한다면
-				cout << "[AUTH] success" << endl;
-				build_response(res, 200);
+				cout << "token success, server on" << endl;
+				build_response(res, 200, "[RES] 200 ok", "token success, server on");
 			}
 			else { //token 값이 일치하지 않으면
-				cout << "[AUTH] unauthorized" << endl;
-				build_response(res, 401);
+				build_response(res, 401, "[AUTH] unauthorized", "unauthorized");
 			}
 		}
 		else { //token이 존재하지 않음
-			cout << "[AUTH] no token";
-			build_response(res, 401);
+			build_response(res, 401, "[AUTH] missing token", "no token");
 		}
 
 	});
+
+	server.Post("/album-records", [](const httplib::Request& req, httplib::Response& res) {
+		cout << "[REQ] POST /album-records" << endl;
+		string body = req.body; 
+
+		//파싱
+		size_t p1 = body.find('/'); //'/'를 기준으로 파싱해 위치함
+		size_t p2 = body.find('/', p1 + 1);
+
+		//잘못된 입력처리
+		if (p1 == string::npos || p2 == string::npos) {
+			build_response(res, 400, "[RES] 400 Bad Request", "bad request");
+			return;
+		}
+
+		//제공된 정보의 임시 문자열
+		string date = body.substr(0, p1); //처음부터 '/' 바로 전까지
+		string album = body.substr(p1 + 1, p2 - p1 - 1); 
+		string artist = body.substr(p2 + 1);
+
+		//중복 검사 (date, album, artist)
+		for (auto& r : AlbumRecords) {
+			if (r.date == date && r.album == album && r.artist == artist) { //모두 중복 시
+				cout << "This album already exists" << endl;
+				build_response(res, 409, "[RES] 409 conflict", "conflict");
+				return;
+			}
+		}
+
+		int id = next_id; //임시 id 생성 후 다음 id 번호 지정
+		next_id++;
+		AlbumRecords.emplace_back(id, date, album, artist); //중복이 아니면 데이터 추가
+		build_response(res, 201, "[RES] 201 created", "created");
+
+	});
+
+	server.Get("/album-records", [](const httplib::Request& req, httplib::Response& res) {
+		cout << "[REQ] GET /album-records" << endl;
+		//기록된 게 없는지(비어 있는지) 확인
+		if (AlbumRecords.empty()) {	//비어 있을 시
+			cout << "no records" << endl;
+			build_response(res, 200, "[RES] 200 ok", "no records");
+			return;
+		}
+
+		string result; //누적시킬 임시 문자열 생성
+
+		//모든 records들의 정보 저장
+		for (auto& record : AlbumRecords) {
+			result += to_string(record.id) + " | "
+				+ record.date + " | "
+				+ record.album + " | "
+				+ record.artist + "\n";
+		}
+
+		build_response(res, 200, "[RES] 200 ok", result);
+	});
+	
 
 	cout << "[LISTEN] waiting for requests on 8080" << endl; //8080번 포트에서 대기할 것이라 선언
 	//서버 실행
